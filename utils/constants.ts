971dc81cc077bc843c202a42aad8db07062bc881
sollmayer <stupanenko.serhii@gmail.com>
2025-06-08 00:49:35 +0300

    add PageRoutes and BASE_URL constants

---
+++ utils/constants.ts
@@ -0,0 +1,7 @@
+export const BASE_URL = process.env.BASE_URL || 'https://ecommerce-playground.lambdatest.io/';
+
+export const PageRoutes = {
+  HOME: 'index.php?route=common/home',
+  LOGIN: 'index.php?route=account/login',
+  REGISTER: 'index.php?route=account/register',
+};

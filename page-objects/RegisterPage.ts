6437f3851c8e8f1a5f1d9a2e0e215036cf3be37c
sollmayer <stupanenko.serhii@gmail.com>
2025-06-09 18:56:26 +0300

    Add user registration and validation methods

--- page-objects/RegisterPage.ts
+++ page-objects/RegisterPage.ts
@@ -1,5 +1,7 @@
 import { Page, Locator, expect } from "@playwright/test";
 import { HeaderComponent } from "./components/HeaderComponent";
+import { PageRoutes } from "../utils/constants";
+import { TestDataGenerator } from "../utils/TestDataGenerator";
 
 export class RegisterPage {
 	private readonly page: Page;
@@ -11,7 +13,6 @@ export class RegisterPage {
 	readonly phoneInput: Locator;
 	readonly passwordInput: Locator;
 	readonly passwordConfirmInput: Locator;
-	readonly newsletterSubscribeRadioButton: Locator[];
 	readonly privacyPolicyCheckbox: Locator;
 
 	readonly continueButton: Locator;
@@ -26,7 +27,59 @@ export class RegisterPage {
 		this.phoneInput = page.getByRole("textbox", { name: "Telephone" });
 		this.passwordInput = page.locator("#input-password");
 		this.passwordConfirmInput = page.locator("#input-confirm");
-		this.privacyPolicyCheckbox = page.locator('#input-agree')
+		this.privacyPolicyCheckbox = page.locator("#input-agree");
+		this.continueButton = page.getByRole("button", { name: "Continue" });
+	}
+	async goto() {
+		await this.page.goto(PageRoutes.REGISTER);
+		await expect(this.pageBreadcrumbs).toBeVisible();
+		await expect(this.pageBreadcrumbs).toContainText(/Account Register/);
 	}
 
+	async fillRegistrationForm(userData: {
+		firstName: string;
+		lastName: string;
+		email: string;
+		telephone: string;
+		password: string;
+	}) {
+		await this.firstNameInput.fill(userData.firstName);
+		await this.lastNameInput.fill(userData.lastName);
+		await this.emailInput.fill(userData.email);
+		await this.phoneInput.fill(userData.telephone);
+		await this.passwordInput.fill(userData.password);
+		await this.passwordConfirmInput.fill(userData.password);
+	}
+
+	async subscribeToNewsletter() {
+		await this.page.locator("input#input-newsletter-yes").check({ force: true });
+	}
+
+	async agreeToPrivacyPolicy() {
+		await this.privacyPolicyCheckbox.check();
+	}
+
+	async registerNewUser(userData: {
+		firstName: string;
+		lastName: string;
+		email: string;
+		telephone: string;
+		password: string;
+	}) {
+		await this.fillRegistrationForm(userData);
+		await this.agreeToPrivacyPolicy();
+		await this.clickContinue();
+	}
+
+	async getFieldValidationError(fieldName: "firstname" | "lastname" | "email" | "telephone" | "password" | "confirm") {
+		return this.page.locator(`#input-${fieldName} + .text-danger`);
+	}
+
+	async getMainWarningMessage() {
+		return this.page.locator(".alert.alert-danger").first();
+	}
+
+	async clickContinue() {
+		await this.continueButton.click();
+	}
 }

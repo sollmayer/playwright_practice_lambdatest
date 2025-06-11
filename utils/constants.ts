b8ae7421f032ab08fe4013c1432cc59fa10f9506
sollmayer <stupanenko.serhii@gmail.com>
2025-06-11 20:57:18 +0300

    Add routes for account_success and my_account pages

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,4 +4,6 @@ export const PageRoutes = {
   HOME: 'index.php?route=common/home',
   LOGIN: 'index.php?route=account/login',
   REGISTER: 'index.php?route=account/register',
+  ACCOUNT_SUCCESS: 'index.php?route=account/success',
+  MY_ACCOUNT: 'index.php?route=account/account',
 };
